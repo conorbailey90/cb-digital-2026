ab1913b13cae4f24d1b5483b117ddad6cb2e5794
Conor Bailey <conbailey90@gmail.com>
2026-01-07 08:22:24 +0000

    Mobile nav added

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,11 +3,11 @@
     import { base } from "$app/paths";
     
     const navigation = [
-        {name: 'Home', link: '/#home'},
-        {name: 'About', link: '/#about'},
-        {name: 'Services', link: '/#services'},
-        {name: 'Projects', link: '/#projects'},
-        {name: 'Contact', link: '/#contact'},
+        {name: 'Home', link: `${base}/#home`},
+        {name: 'About', link: `${base}/#about`},
+        {name: 'Services', link: `${base}/#services`},
+        {name: 'Projects', link: `${base}/#projects`},
+        {name: 'Contact', link: `${base}/#contact`},
     ];
 
     const socials = [
